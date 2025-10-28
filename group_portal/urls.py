628004095bb93af576ca4a409f09c7bd75d5d9e9
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-10-28 21:01:06 +0200

    Кастомізував адмін-панель і додав іконки

--- group_portal/urls.py
+++ group_portal/urls.py
@@ -35,4 +35,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
+admin.site.site_header = "Портал групи"
+admin.site.site_title = "Адмінка Сайту"
+admin.site.index_title = "Ласкаво просимо до панелі Порталу групи"
+admin.site.icon = ""
--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Project, ProjectImage
 
-admin.site.register(Project)
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    search_fields = ('title', 'user__username')
+    list_filter = ('created_at',)
+    fieldsets = (
+        ('Інформація про проеєкт', {
+            'fields': ('user', 'title', 'desc')
+        }),
+        ('Фали та зображення', {
+            'fields': ('link', 'file')
+        })
+    )
+
+
 admin.site.register(ProjectImage)
